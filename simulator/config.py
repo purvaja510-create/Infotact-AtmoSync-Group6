a21d25e6e596ab7be30a512159a4bc9d6f1ef9ea
bhanuprasad121 <bhanuprasadpujari2000@gmail.com>
2026-07-10 16:19:49 +0530

    genrated python lot

---
+++ simulator/config.py
@@ -0,0 +1,56 @@
+Containers=[
+"A101",
+"A102",
+"A103",
+"A104",
+"A105",
+"A106"
+]
+
+
+Commodities= {
+    "Avocado": {
+        "temp_range": (4,8),
+        "humidity_range": (65,80),
+        "vibration_range":(0.10, 1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    },
+    "Banana":{
+        "temp_range":(13,15),
+        "humidity_range":(85,95),
+        "vibration_range":(0.10,1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    },
+    "Mango":{
+        "temp_range":(10,13),
+        "humidity_range":(80,90),
+        "vibration_range":(0.10,1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    },
+    "Medicine":{
+        "temp_range":(2,8),
+        "humidity_range":(30,50),
+        "vibration_range":(0.10,1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    },
+    "Vaccine":{
+        "temp_range":(2,8),
+        "humidity_range":(30,50),
+        "vibration_range":(0.10,1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    },
+    "Vegitables":{
+        "temp_range":(10,16),   
+        "humidity_range":(50,60),
+        "vibration_range":(0.10,1.00),
+        "latitude_range":(17.30,17.50),
+        "longitude_range":(78.30, 78.60)
+    }
+}    
+
+interval=5
