f3c15b731c95bbd91420fe2ac6ea9a87860baee8
Purvaja Dodke <purvaja510@gmail.com>
2026-07-20 18:00:23 +0530

    Refine fact model to leverage route dimension

--- atmosync_dbt/models/marts/fact_sensor_readings.sql
+++ atmosync_dbt/models/marts/fact_sensor_readings.sql
@@ -1,24 +1,40 @@
 SELECT
+
+
     s.timestamp,
+
     s.container_id,
+
     dc.commodity,
+
     cp.category,
+
     cp.price_per_kg,
 
-    CONCAT(dc.source, '_', dc.destination) AS route_key,
-    dc.source,
+    dr.route_key,
 
-    dc.destination,
+    dr.source,
+
+    dr.destination,
+
     s.temperature,
+
     s.humidity,
+
     s.vibration,
+
     s.battery_level,
+
     s.speed
 
 FROM {{ ref('stg_sensor_readings') }} s
 
 LEFT JOIN {{ ref('dim_container') }} dc
     ON s.container_id = dc.container_id
+
+LEFT JOIN {{ ref('dim_route') }} dr
+    ON dc.source = dr.source
+   AND dc.destination = dr.destination
 
 LEFT JOIN {{ source('raw', 'commodity_pricing') }} cp
     ON dc.commodity = cp.commodity
