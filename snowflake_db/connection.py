9ebff8c0b55acf996ff6624125ab84818aa432cd
Purvaja Dodke <purvaja510@gmail.com>
2026-08-12 21:41:31 +0530

    Protected Snowflake connection password

--- snowflake_db/connection.py
+++ snowflake_db/connection.py
@@ -4,7 +4,7 @@ import snowflake.connector
 def get_connection():
     conn = snowflake.connector.connect(
         user="PURVAJA510",
-        password="Purvajadodke@1234",
+        password="xyz@1234",
         account="ALHXHWY-BD79190",
         warehouse="COMPUTE_WH",
         database="ATMOSYNC_DB",
